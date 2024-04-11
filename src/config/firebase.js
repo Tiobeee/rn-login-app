3b05941cdee1c3ce946dcbf913deb6014510ff32
Tiobee <bernardo.budal10@gmail.com>
2024-04-11 16:43:49 -0300

    First Commitadas

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnvwBp3mOFtEWTr3YAEAeiBQhIIUhuQiw",
+  authDomain: "applogin-bf7eb.firebaseapp.com",
+  projectId: "applogin-bf7eb",
+  storageBucket: "applogin-bf7eb.appspot.com",
+  messagingSenderId: "40912500560",
+  appId: "1:40912500560:web:fc32d5ac3cc134bb7f5e55"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ src/navigation/AppNavigator.jsx
@@ -0,0 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+            name="HomeScreen" 
+            component={HomeScreen}
+            options={
+                {
+                    title: "Tela Inicial",
+                }
+            }
+        />
+        <Stack.Screen 
+            name="LoginScreen" 
+            component={LoginScreen} 
+            options={{
+                title: "Login",
+            }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
