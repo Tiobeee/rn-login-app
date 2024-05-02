6aa4514cc43c41f707a20c8cbcaf21c1b5d53571
Tiobee <bernardo.budal10@gmail.com>
2024-05-02 16:51:47 -0300

    Nada

--- App.js
+++ App.js
@@ -2,16 +2,25 @@ import { MD3LightTheme, Provider } from "react-native-paper";
 import AppNavigator from "./src/navigation/AppNavigator";
 
 // note que criamos o arquivo src/config/theme.js
-import { themeLight } from "./src/config/theme";
+import { themeDark, themeLight } from "./src/config/theme";
+import { useColorScheme } from "react-native";
 
 export default function App() {
+  // pega o tema do dispositivo
+  const colorScheme = useColorScheme();
   // criação de tema
   // https://callstack.github.io/react-native-paper/docs/guides/theming/#creating-dynamic-theme-colors
-
-  const theme = {
-    ...MD3LightTheme, // pega o tema padrão do RNP
-    colors: themeLight.colors, // sobrescreve as cores do tema
-  };
+  const isDarkMode = colorScheme === "dark";
+  
+  // let theme;
+  // if (isDarkMode) {
+  //   theme = themeDark;
+  // } else {
+  //   theme = themeLight;
+  // }
+  
+  // operador ternário
+  const theme = isDarkMode ? themeDark : themeLight;
 
   return (
     <Provider theme={theme}>
